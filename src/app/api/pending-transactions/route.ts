7becd3426f9d30379e173657d4dce708666cb6c9
visal1234 <bzhou@paragoniu.edu.kh>
2024-05-11 00:22:05 +0700

    fnish function get in /pendingTransaction

--- src/app/api/pending-transactions/route.ts
+++ src/app/api/pending-transactions/route.ts
@@ -1,4 +1,9 @@
 import { NextResponse } from "next/server";
 import prismaClient from "@/lib/prisma";
 
-export async function GET() {}
+export async function GET() {
+    let pendingTransactions = await prismaClient.pendingTransaction.findMany({
+        orderBy: { timestamp: "asc" },
+    });
+    return NextResponse.json({ pendingTransactions });
+}
