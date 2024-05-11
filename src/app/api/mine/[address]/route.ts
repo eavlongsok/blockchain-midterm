74f1780f4470b0ed61eea6e47418c03db3b3544b
visal1234 <bzhou@paragoniu.edu.kh>
2024-05-12 01:57:05 +0700

    minor changer

--- src/app/api/mine/[address]/route.ts
+++ src/app/api/mine/[address]/route.ts
@@ -1,7 +1,7 @@
+import { NextResponse } from "next/server";
 import { BlockchainClass } from "@/app/type";
 import prismaClient from "@/lib/prisma";
-import { getBlocksAsClass, getPendingTransactionsAsClass } from "@/lib/utils";
-import { NextResponse } from "next/server";
+import { getPendingTransactionsAsClass, getBlocksAsClass } from "@/lib/utils";
 
 export async function POST(
     request: Request,
@@ -9,36 +9,52 @@ export async function POST(
 ) {
     const { address } = context.params;
 
-    const pendingTransactions = await getPendingTransactionsAsClass();
-    if (pendingTransactions.length === 0) {
+    try {
+        // Get pending transactions
+        const pendingTransactions = await getPendingTransactionsAsClass();
+
+        // If no pending transactions, return error
+        if (pendingTransactions.length === 0) {
+            return NextResponse.json({
+                success: false,
+                message: "There are no pending transactions to mine",
+            });
+        }
+
+        // Get blocks
+        const blocks = await getBlocksAsClass();
+
+        // Create blockchain instance
+        const blockchain = new BlockchainClass(blocks);
+
+        // Mine pending transactions
+        blockchain.minePendingTransactions(address);
+
+        // Get the latest mined block
+        const latestMinedBlock = blockchain.getLatestBlock();
+
+        // Insert new block into the database
+        await prismaClient.blockchain.create({
+            data: {
+                timestamp: new Date(latestMinedBlock.timestamp),
+                previousHash: latestMinedBlock.previousHash,
+                hash: latestMinedBlock.hash,
+                data: JSON.stringify(latestMinedBlock.transactions),
+                nonce: latestMinedBlock.nonce,
+            },
+        });
+
+        // Return success response
+        return NextResponse.json({
+            success: true,
+            message: "Block mined successfully",
+            block: latestMinedBlock,
+        });
+    } catch (error) {
+        // Return error response
         return NextResponse.json({
             success: false,
+            message: "Block mined Failed",
         });
     }
-
-    const blocks = await getBlocksAsClass();
-
-    const blockchain = new BlockchainClass(blocks);
-    const blockNumberBeforeMining = blockchain.chain.length;
-    blockchain.pendingTransactions = pendingTransactions;
-    blockchain.minePendingTransactions(address);
-
-    const latestMinedBlock = blockchain.getLatestBlock();
-
-    const blockNumberAfterMining = blockchain.chain.length;
-
-    // insert new block into database
-    await prismaClient.blockchain.create({
-        data: {
-            timestamp: new Date(latestMinedBlock.timestamp),
-            previousHash: latestMinedBlock.previousHash,
-            hash: latestMinedBlock.hash,
-            data: JSON.stringify(latestMinedBlock.transactions),
-            nonce: latestMinedBlock.nonce,
-        },
-    });
-
-    return NextResponse.json({
-        success: blockNumberAfterMining > blockNumberBeforeMining,
-    });
 }
